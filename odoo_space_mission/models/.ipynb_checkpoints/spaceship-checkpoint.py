a08bf5686c20921a4faed7440ef5412a9ae0f7f0
Joseph <waalo.ecuador@gmail.com>
2021-11-02 19:00:18 +0000

    [ADD] odoo_space_mission: added fields

--- odoo_space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
+++ odoo_space_mission/models/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -5,3 +5,26 @@ from odoo import models, fields, api
 class Spaceship(models.Model):
     _name = "space_mission.spaceship"
     _description = "Odoo's spaceship model"
+    
+    name = fields.Char("Name", required=True)
+    dimensions = fields.Float("Dimension" help="Dimension expressed in cubic meters")
+    fuel_type = fields.Selection(
+        "Fuel Type",
+        selection=[
+            ('liquid', 'Liquid'),
+            ('solid', 'Solid'),
+            ('hydrogen', 'Liquid Hydrogen'),
+        ]
+        copy=False,
+    )
+    ship_type = fields.Selection(
+        "Ship Type",
+        selection=[
+            ('flyby', 'Flyby spacecraft'),
+            ('orbiter', 'Orbiter spacecraft'),
+            ('lander', 'Lander spacecraft'),
+        ]
+        copy=False,
+    )
+    passengers_number = fields.Integer("Number of passengers")
+    active = fields.Boolean("Active", default=True)
