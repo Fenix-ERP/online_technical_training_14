afcfc5f3343f50851749650788af58a7b45aa3a7
Joseph <waalo.ecuador@gmail.com>
2021-11-02 19:57:49 +0000

    [ADD] library_management and cooperative_volunteers

--- cooperative_volunteers/models/.ipynb_checkpoints/task-checkpoint.py
+++ cooperative_volunteers/models/.ipynb_checkpoints/task-checkpoint.py
@@ -5,3 +5,17 @@ from odoo import models, fields, api
 class Task(models.Model):
     _name = "volunteers.task"
     _description = "Task model to split the work"
+    
+    name = fields.Char("Name", required=True)
+    date_start = fields.Date(string="Start Task")
+    date_stop = fields.Date(string="Stop Task")
+    repeat = fields.Boolean(string="Repeats")
+    frequency = fields.Char(string="frequency:")
+    task_type = fields.Selection(
+        string="Task Type",
+        selection=[
+            ('practice', 'Practice'),
+            ('theoretical', 'Theoretical')
+        ],
+        copy=False
+    )
--- library_management/models/.ipynb_checkpoints/book-checkpoint.py
+++ library_management/models/.ipynb_checkpoints/book-checkpoint.py
@@ -5,3 +5,18 @@ from odoo import models, fields, api
 class Book(models.Model):
     _name = "library.book"
     _description = "Book model for library management"
+    
+    name = fields.Char("Name", required=True)
+    authors = fields.Text("Authors", required=True)
+    editors = fields.Char("Editors", required=True)
+    publisher = fields.Char("Publisher", required=True)
+    year_of_edition = fields.Date(string="Year of Edition")
+    isbn = fields.Char("ISBN", required=True)
+    genere = fields.Selection([
+        ('dramatic', 'Dramatic'),
+        ('terror', 'Terror'),
+        ('comedia', 'Comedy')
+    ],"Genere", required=True)
+    
+    
+   
