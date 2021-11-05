b81aef0416299b80d639c6a6b66e7d7b33a26c86
Joseph <waalo.ecuador@gmail.com>
2021-11-05 17:34:52 +0000

    [ADD] odoo_space_mission: ORM and fixed demo data

--- odoo_space_mission/models/spaceship.py
+++ odoo_space_mission/models/spaceship.py
@@ -2,12 +2,16 @@
 
 from odoo import models, fields, api
 
+from odoo.exceptions import UserError
+
 class Spaceship(models.Model):
     _name = "space.spaceship"
     _description = "Odoo's spaceship model"
     
     name = fields.Char("Name", required=True)
-    dimensions = fields.Float("Dimension", help="Dimension expressed in cubic meters")
+    length = fields.Float("Length", required=True, default=0)
+    width = fields.Float("Width", required=True, default=0)
+    dimension = fields.Float("Dimension", compute="_compute_dimension", readonly=True)
     fuel_type = fields.Selection(
         string="Fuel Type",
         selection=[
@@ -28,3 +32,15 @@ class Spaceship(models.Model):
     )
     passengers_number = fields.Integer("Number of passengers")
     active = fields.Boolean("Active", default=True)
+    
+    @api.depends('length','width')
+    def _compute_dimension(self):
+        for record in self:
+            dimension = record.length * record.width
+            record.write({'dimension': dimension})
+            
+    @api.constrains('length','width')
+    def _constrain_dimension(self):
+        for record in self:
+            if width > length:
+                raise UserError("The width of the spaceship can't be bigger than the length")
